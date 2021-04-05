e45ce2b798afa60e3d9e9efd21cc475c7854ab22
Leguark <varga@cgre.rwth-aachen.de>
2021-04-05 16:17:59 +0200

    Preparing TODOs for next features

---
+++ gempy_engine/integrations/interp_manager/interp_manager_api.py
@@ -0,0 +1,16 @@
+from gempy_engine.integrations.interp_single.interp_single_interface import interpolate_single_scalar
+
+
+def interpolate_model(all_the_input):
+
+    # TODO: [ ] Looping scalars
+    s = interpolate_single_scalar
+
+    # --------------------
+    # This should happen only on the leaf of an octree
+    # TODO: [ ] Dual contouring. This method only make one vertex per voxel. It is possible to make water tight surfaces?
+
+    # ---------------------
+    # TODO: [ ] Gravity
+
+    # TODO: [ ] Magnetics
--- gempy_engine/integrations/interp_single/interp_single_interface.py
+++ gempy_engine/integrations/interp_single/interp_single_interface.py
@@ -1,5 +1,6 @@
 from ...core import data
 from ...core.data import exported_structs
+from ...modules.activator.activator_interface import activate_formation_block
 from ...modules.data_preprocess import data_preprocess_interface
 from ...modules.kernel_constructor import kernel_constructor_interface
 from gempy_engine.modules.solver import solver_interface
@@ -7,9 +8,15 @@ from gempy_engine.modules.solver import solver_interface
 import numpy as np
 
 
-def interpolate_single_scalar(surface_points: data.SurfacePoints, orientations: data.Orientations, grid: np.ndarray,
-                              options: data.InterpolationOptions, data_shape: data.TensorsStructure):
-    sp_internal = data_preprocess_interface.prepare_surface_points(surface_points, data_shape.number_of_points_per_surface)
+def interpolate_single_scalar(surface_points: data.SurfacePoints,
+                              orientations: data.Orientations,
+                              grid: np.ndarray,
+                              unit_values: np.ndarray,
+                              options: data.InterpolationOptions,
+                              data_shape: data.TensorsStructure):
+    # Within series
+    sp_internal = data_preprocess_interface.prepare_surface_points(surface_points,
+                                                                   data_shape.number_of_points_per_surface)
     ori_internal = data_preprocess_interface.prepare_orientations(orientations)
     grid_internal = data_preprocess_interface.prepare_grid(grid, surface_points)
 
@@ -18,55 +25,59 @@ def interpolate_single_scalar(surface_points: data.SurfacePoints, orientations: 
 
     weights = solver_interface.kernel_reduction(A_matrix, b_vector, smooth=0.01)  # TODO: Smooth should be taken from options
 
+    # Within octree level
     exported_fields = _evaluate_sys_eq(grid_internal, options, ori_internal, sp_internal, weights)
-    scalar_at_surface_points = _get_scalar_field_at_surface_points(exported_fields.scalar_field,
-                                                                   data_shape.nspv,
-                                                                   surface_points.n_points
-                                                                   )
-    output = exported_structs.Output(exported_fields, scalar_at_surface_points)
+
+    scalar_at_surface_points = _get_scalar_field_at_surface_points(
+        exported_fields.scalar_field, data_shape.nspv, surface_points.n_points)
 
     # -----------------
-    # TODO: [ ] Export block
-
-    # TODO: [ ] Topology
-
-    # TODO: [ ] Octree
-    # ------------------
-    # TODO: [ ] Dual contouring
-
-
+    # Export and Masking operations can happen even in parallel
+    # TODO: [~X] Export block
+    values_block = activate_formation_block(exported_fields.scalar_field, scalar_at_surface_points,
+                                            unit_values, sigmoid_slope=50000)
 
     # TODO: [ ] Masking OPs
+    # mask_matrix = mask_matrix(exported_fields.scalar_field, scalar_at_surface_points, some_sort_of_array_with_erode_onlap)
 
-    # ---------------------
+    #-----------------
+    # TODO: [ ] Topology
 
-    # TODO: [ ] Gravity
 
-    # TODO: [ ] Magnetics
+
+    # TODO: [ ] Octree: Define new grid
+
+    # ------------------
+
+
+    output = exported_structs.Output(exported_fields, scalar_at_surface_points)
     return output
+
 
 def _evaluate_sys_eq(grid, options, ori_internal, sp_internal, weights) -> exported_structs.ExportedFields:
     eval_kernel = kernel_constructor_interface.yield_evaluation_kernel(grid, sp_internal, ori_internal, options)
-    eval_gx_kernel = kernel_constructor_interface.yield_evaluation_grad_kernel(grid, sp_internal, ori_internal, options, axis=0)
-    eval_gy_kernel = kernel_constructor_interface.yield_evaluation_grad_kernel(grid, sp_internal, ori_internal, options, axis=1)
+    eval_gx_kernel = kernel_constructor_interface.yield_evaluation_grad_kernel(grid, sp_internal, ori_internal, options,
+                                                                               axis=0)
+    eval_gy_kernel = kernel_constructor_interface.yield_evaluation_grad_kernel(grid, sp_internal, ori_internal, options,
+                                                                               axis=1)
 
     scalar_field = weights @ eval_kernel
     gx_field = weights @ eval_gx_kernel
     gy_field = weights @ eval_gy_kernel
     if options.number_dimensions == 3:
-        eval_gz_kernel = kernel_constructor_interface.yield_evaluation_grad_kernel(grid, sp_internal, ori_internal, options, axis=2)
+        eval_gz_kernel = kernel_constructor_interface.yield_evaluation_grad_kernel(grid, sp_internal, ori_internal,
+                                                                                   options, axis=2)
         gz_field = weights @ eval_gz_kernel
     elif options.number_dimensions == 2:
         gz_field = None
     else:
         raise ValueError("Number of dimensions have to be 2 or 3")
 
-    # TODO: Add here the logic to extract the scalar field value at surface point
-
     return exported_structs.ExportedFields(scalar_field, gx_field, gy_field, gz_field)
 
 
-def _get_scalar_field_at_surface_points(Z_x: np.ndarray, number_of_points_per_surface: np.ndarray, n_surface_points: int):
+def _get_scalar_field_at_surface_points(Z_x: np.ndarray, number_of_points_per_surface: np.ndarray,
+                                        n_surface_points: int):
     npf = number_of_points_per_surface
     scalar_field_at_surface_points_values = Z_x[-n_surface_points:][npf]
 
