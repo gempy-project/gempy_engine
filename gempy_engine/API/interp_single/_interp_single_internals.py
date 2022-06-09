- [BUG] Maybe interpolating gradients bug fixed?

gempy_engine/API/interp_single/_interp_single_internals.py
@@ -221,8 +221,7 @@ def _input_preprocess(data_shape: TensorsStructure, grid, orientations, surface_
     return grid_internal, ori_internal, sp_internal
 
 
-def _evaluate_sys_eq(xyz: np.ndarray, interp_input: SolverInput,
-                     weights: np.ndarray) -> ExportedFields:
+def _evaluate_sys_eq(xyz: np.ndarray, interp_input: SolverInput, weights: np.ndarray) -> ExportedFields:
     options = interp_input.options
 
     eval_kernel = kernel_constructor.yield_evaluation_kernel(xyz, interp_input)
@@ -252,8 +251,7 @@ def _get_scalar_field_at_surface_points(Z_x: np.ndarray, number_of_points_per_su
     return scalar_field_at_surface_points_values
 
 
-def _set_scalar_field_at_surface_points(exported_fields: ExportedFields,
-                                        number_of_points_per_surface: np.ndarray,
+def _set_scalar_field_at_surface_points(exported_fields: ExportedFields, number_of_points_per_surface: np.ndarray,
                                         n_surface_points: int):
     exported_fields.n_points_per_surface = number_of_points_per_surface
     exported_fields.n_surface_points = n_surface_points

gempy_engine/modules/kernel_constructor/_covariance_assembler.py
@@ -14,6 +14,7 @@ tensor_types = BackendTensor.tensor_types
 # TODO: Move this to its right place
 euclidean_distances = True
 
+
 @dataclass
 class InternalDistancesMatrices:
     dif_ref_ref: np.ndarray
@@ -29,7 +30,8 @@ class InternalDistancesMatrices:
     r_rest_rest: np.ndarray
     hu_ref: np.ndarray
     hu_rest: np.ndarray
-
+    hu_ref_grad: np.ndarray
+    hu_rest_grad: np.ndarray
 
 
 def create_cov_kernel(ki: KernelInput, options: InterpolationOptions) -> tensor_types:
@@ -38,7 +40,6 @@ def create_cov_kernel(ki: KernelInput, options: InterpolationOptions) -> tensor_
     c_o = options.c_o
     i_magic = options.i_res
     gi_magic = options.gi_res
-
 
     # Calculate euclidean or square distances depending on the function kernel
     global euclidean_distances
@@ -68,7 +69,7 @@ def _get_cov(c_o, dm, k_a, k_p_ref, k_p_rest, k_ref_ref, k_ref_rest, k_rest_ref,
     usp = _get_universal_sp_terms(ki, options)
     ug = _get_universal_gradient_terms(ki, options)
     drift = usp + ug
-    
+
     cov = c_o * (cov_grad + cov_sp + cov_grad_sp) + drift  # *  NOTE: (miguel) The magic terms are real and now they are already included
     return cov
 
@@ -90,8 +91,8 @@ def create_scalar_kernel(ki: KernelInput, options: InterpolationOptions) -> tens
     k_a, k_p_ref, k_p_rest, k_ref_ref, k_ref_rest, k_rest_ref, k_rest_rest = \
         _compute_all_kernel_terms(a, kernel_f, dm.r_ref_ref, dm.r_ref_rest, dm.r_rest_ref, dm.r_rest_rest)
 
-    sigma_0_sp = - options.i_res * (k_rest_rest - k_ref_ref) # This are right terms
-    sigma_0_grad_sp = options.gi_res * (dm.hu_ref * k_p_ref) #dm.huv_ref * k_p_ref # this are the right terms
+    sigma_0_sp = - options.i_res * (k_rest_rest - k_ref_ref)  # This are right terms
+    sigma_0_grad_sp = options.gi_res * (dm.hu_ref * k_p_ref)  # dm.huv_ref * k_p_ref # this are the right terms
 
     # U_sp
     usp_ref = (ki.ref_drift.dipsPoints_ui_ai * ki.ref_drift.dipsPoints_ui_aj).sum(axis=-1)
@@ -103,7 +104,6 @@ def create_scalar_kernel(ki: KernelInput, options: InterpolationOptions) -> tens
     selector = (ki.drift_matrix_selector.sel_ui * (ki.drift_matrix_selector.sel_vj + 1)).sum(-1)
 
     drift = selector * (options.gi_res * usp_ref + options.i_res * usp_ref_d2)
-
 
     return c_o * (sigma_0_sp + sigma_0_grad_sp) + drift
 
@@ -126,7 +126,7 @@ def create_grad_kernel(ki: KernelInput, options: InterpolationOptions) -> tensor
         _compute_all_kernel_terms(a, kernel_f, dm.r_ref_ref, dm.r_ref_rest, dm.r_rest_ref, dm.r_rest_rest)
 
     sigma_0_grad = (+1) * dm.hu * dm.hv / (dm.r_ref_ref ** 2 + 1e-5) * (- k_p_ref + k_a) - k_p_ref * dm.perp_matrix
-    sigma_0_sp_grad =  dm.huv_ref * k_p_ref -  dm.huv_rest * k_p_rest
+    sigma_0_sp_grad = -options.gi_res * (dm.hu_ref_grad * k_p_ref - dm.hu_rest_grad * k_p_rest)
 
     # region drift
 
@@ -136,20 +136,18 @@ def create_grad_kernel(ki: KernelInput, options: InterpolationOptions) -> tensor
     ug2 = (ki.ori_drift.dips_ug_bi * ki.ori_drift.dips_ug_bj).sum(axis=-1)
     # Third term
     ug3_aux = (ki.ori_drift.dips_ug_ci * ki.ori_drift.dips_ug_cj).sum(axis=-1)
-    third_term_selector =  (ki.ori_drift.selector_ci * ki.ori_drift.dips_ug_aj).sum(axis=-1)
+    third_term_selector = (ki.ori_drift.selector_ci * ki.ori_drift.dips_ug_aj).sum(axis=-1)
     ug3 = ug3_aux * third_term_selector
 
     selector = (ki.drift_matrix_selector.sel_ui * (ki.drift_matrix_selector.sel_vj + 1)).sum(-1)
-    total_ug = selector * (ug + options.gi_res * ug2 + options.gi_res * ug3)
+    drift = selector * (ug + options.gi_res * ug2 + options.gi_res * ug3)
 
     # endregion
 
-    return c_o * (sigma_0_grad + sigma_0_sp_grad) + total_ug
-
+    return c_o * (sigma_0_grad + sigma_0_sp_grad) + drift
 
 
 def _compute_all_kernel_terms(a: int, kernel_f: KernelFunction, r_ref_ref, r_ref_rest, r_rest_ref, r_rest_rest):
-
     k_rest_rest = kernel_f.base_function(r_rest_rest, a)
     k_ref_ref = kernel_f.base_function(r_ref_ref, a)
     k_ref_rest = kernel_f.base_function(r_ref_rest, a)
@@ -168,14 +166,21 @@ def _compute_all_distance_matrices(cs, ori_sp_matrices) -> InternalDistancesMatr
     hv = -(dif_ref_ref * (cs.hv_sel_i * cs.hv_sel_j)).sum(axis=-1)  # C
 
     hu_ref = dif_ref_ref * (cs.hu_sel_i * cs.h_sel_ref_j)
+    test_foo = hu_ref.sum(-1)
+
     hv_ref = dif_ref_ref * (cs.h_sel_ref_i * cs.hv_sel_j)
-    huv_ref =  hu_ref.sum(axis=-1) - hv_ref.sum(axis=-1)  # C
+    huv_ref = hu_ref.sum(axis=-1) - hv_ref.sum(axis=-1)  # C
 
     hu_rest = dif_rest_rest * (cs.hu_sel_i * cs.h_sel_rest_j)
     hv_rest = dif_rest_rest * (cs.h_sel_rest_i * cs.hv_sel_j)
     huv_rest = hu_rest.sum(axis=-1) - hv_rest.sum(axis=-1)  # C
-
+    
     perp_matrix = (cs.hu_sel_i * cs.hv_sel_j).sum(axis=-1)
+    
+    # For gradients
+    hu_ref_grad = (dif_ref_ref * (cs.h_sel_ref_i * cs.hu_sel_j)).sum(axis=-1)
+    hu_rest_grad = (dif_rest_rest * (cs.h_sel_ref_i * cs.hu_sel_j)).sum(axis=-1)
+    
     if BackendTensor.pykeops_enabled is True:
 
         r_ref_ref = dif_ref_ref.sqdist(dif_ref_ref)
@@ -201,9 +206,10 @@ def _compute_all_distance_matrices(cs, ori_sp_matrices) -> InternalDistancesMatr
         perp_matrix,
         r_ref_ref, r_ref_rest, r_rest_ref, r_rest_rest,
         hu_ref.sum(axis=-1),
-        hu_rest.sum(axis=-1)
+        hu_rest.sum(axis=-1),
+        hu_ref_grad,
+        hu_rest_grad
     )
-
 
 
 def _get_universal_sp_terms(ki, options):
@@ -220,7 +226,6 @@ def _get_universal_sp_terms(ki, options):
     usp_rest_d2b = (ki.rest_drift.dipsPoints_ui_bi1 * ki.rest_drift.dipsPoints_ui_bj1).sum(axis=-1)
     usp_rest_d2c = (ki.rest_drift.dipsPoints_ui_bi2 * ki.rest_drift.dipsPoints_ui_bj2).sum(axis=-1)
     usp_rest_d2 = usp_rest_d2b * usp_rest_d2c
-
 
     selector = (ki.drift_matrix_selector.sel_ui * (ki.drift_matrix_selector.sel_vj + 1)).sum(-1)
     usp_d2 = -1 * selector * ((options.i_res * (usp_rest_d2 - usp_ref_d2)) + (options.gi_res * (usp_rest - usp_ref)))
@@ -267,7 +272,7 @@ def _test_covariance_items(ki: KernelInput, options: InterpolationOptions, item)
     elif item == "cov_grad_sp":
         cov_grad_sp = _get_cross_cov_grad_sp(dm, k_p_ref, k_p_rest, options)
 
-        return cov_grad_sp # - dm.huv_rest * k_p_rest + dm.huv_ref * k_p_ref
+        return cov_grad_sp  # - dm.huv_rest * k_p_rest + dm.huv_ref * k_p_ref
 
     elif item == "drift_eval":
         usp_ref = (ki.ref_drift.dipsPoints_ui_ai * ki.ref_drift.dipsPoints_ui_aj).sum(axis=-1)
@@ -283,7 +288,7 @@ def _test_covariance_items(ki: KernelInput, options: InterpolationOptions, item)
     elif item == "drift_usp":
         usp_d2 = _get_universal_sp_terms(ki, options)
 
-        return  usp_d2
+        return usp_d2
 
     elif item == "drift":
         usp_ref = (ki.ref_drift.dipsPoints_ui_ai * ki.ref_drift.dipsPoints_ui_aj).sum(axis=-1)
@@ -296,13 +301,13 @@ def _test_covariance_items(ki: KernelInput, options: InterpolationOptions, item)
         cov = _get_cov(c_o, dm, k_a, k_p_ref, k_p_rest, k_ref_ref, k_ref_rest, k_rest_ref, k_rest_rest, ki, options)
         return cov
 
-    elif item =="sigma_0_sp":
-        return c_o * (k_rest_rest - k_ref_ref) # These are right terms
+    elif item == "sigma_0_sp":
+        return c_o * (k_rest_rest - k_ref_ref)  # These are right terms
 
-    elif item =="sigma_0_grad_sp":
-        return c_o * ( dm.hu_ref * k_p_ref) # These are right terms
+    elif item == "sigma_0_grad_sp":
+        return c_o * (dm.hu_ref * k_p_ref)  # These are right terms
 
-    elif item=="sigma_0_u_sp":
+    elif item == "sigma_0_u_sp":
         usp_ref = (ki.ref_drift.dipsPoints_ui_ai * ki.ref_drift.dipsPoints_ui_aj).sum(axis=-1)
 
         usp_ref_d2b = (ki.ref_drift.dipsPoints_ui_bi1 * ki.ref_drift.dipsPoints_ui_bj1).sum(axis=-1)

gempy_engine/modules/kernel_constructor/_kernel_selectors.py
@@ -5,7 +5,7 @@ def dips_sp_cartesian_selector(vector_size, n_dim, n_dips, n_points, axis: int =
     sel_hu = np.zeros((vector_size, n_dim))
     sel_hv = np.zeros((vector_size, n_dim))
     sel_huv = np.zeros((vector_size, n_dim))
-    for i in range(n_dim):
+    for i in range(n_dim): # ! sel_hu has to be the same also for all axis 
         sel_hu[n_dips * i:n_dips * (i + 1), i] = 1
 
     sel_hv[:n_dips * n_dim, :] = 1

gempy_engine/modules/kernel_constructor/_structs.py
@@ -51,7 +51,6 @@ class OrientationsDrift:
                  x_degree_2: np.ndarray, y_degree_2: np.ndarray,
                  x_degree_2b: np.ndarray, y_degree_2b: np.ndarray,
                  selector_degree_2: np.ndarray):
-
         self.dips_ug_ai = x_degree_1[:, None, :]
         self.dips_ug_aj = y_degree_1[None, :, :]
         self.dips_ug_bi = x_degree_2[:, None, :]
@@ -61,9 +60,9 @@ class OrientationsDrift:
         self.selector_ci = selector_degree_2[:, None, :]
         self.selector_cj = selector_degree_2[None, :, :]
 
-
         if BackendTensor.engine_backend == AvailableBackends.numpy and BackendTensor.pykeops_enabled:
             _upgrade_kernel_input_to_keops_tensor(self)
+
 
 @dataclass
 class PointsDrift:
@@ -85,6 +84,7 @@ class PointsDrift:
         if BackendTensor.engine_backend == AvailableBackends.numpy and BackendTensor.pykeops_enabled:
             _upgrade_kernel_input_to_keops_tensor(self)
 
+
 @dataclass
 class CartesianSelector:
     hu_sel_i: tensor_types = np.empty((0, 1, 3))
@@ -98,8 +98,14 @@ class CartesianSelector:
     h_sel_rest_i: tensor_types = np.empty((0, 1, 3))
     h_sel_rest_j: tensor_types = np.empty((1, 0, 3))
 
-    def __init__(self, x_sel_hu, y_sel_hu, x_sel_hv, y_sel_hv, x_sel_h_ref,
-                 y_sel_h_ref, x_sel_h_rest, y_sel_h_rest):
+    is_gradient: bool = False
+
+    def __init__(self,
+                 x_sel_hu, y_sel_hu,
+                 x_sel_hv, y_sel_hv,
+                 x_sel_h_ref, y_sel_h_ref,
+                 x_sel_h_rest, y_sel_h_rest,
+                 is_gradient=False):
         self.hu_sel_i = x_sel_hu[:, None, :]
         self.hu_sel_j = y_sel_hu[None, :, :]
 
@@ -111,16 +117,20 @@ class CartesianSelector:
 
         self.h_sel_rest_i = x_sel_h_rest[:, None, :]
         self.h_sel_rest_j = y_sel_h_rest[None, :, :]
+
+        self.is_gradient = is_gradient
+
         if BackendTensor.engine_backend == AvailableBackends.numpy and BackendTensor.pykeops_enabled:
             _upgrade_kernel_input_to_keops_tensor(self)
+
 
 @dataclass
 class DriftMatrixSelector:
     sel_ui: tensor_types = np.empty((0, 1, 3))
 
     # TODO: Check these are unused
-    #sel_uj: tensor_types = np.empty((1, 0, 3))
-    #sel_vi: tensor_types = np.empty((0, 1, 3))
+    # sel_uj: tensor_types = np.empty((1, 0, 3))
+    # sel_vi: tensor_types = np.empty((0, 1, 3))
     sel_vj: tensor_types = np.empty((1, 0, 3))
 
     def __init__(self, x_size: int, y_size: int, n_drift_eq: int):
@@ -134,13 +144,14 @@ class DriftMatrixSelector:
         sel_j[-n_drift_eq:, 1] = -1
 
         self.sel_ui = sel_i[:, None, :]
-     #   self.sel_uj = sel_j[None, :, :]
+        #   self.sel_uj = sel_j[None, :, :]
 
-     #   self.sel_vi = -sel_j[:, None, :]
+        #   self.sel_vi = -sel_j[:, None, :]
         self.sel_vj = sel_j[None, :, :]
 
         if BackendTensor.engine_backend == AvailableBackends.numpy and BackendTensor.pykeops_enabled:
             _upgrade_kernel_input_to_keops_tensor(self)
+
 
 @dataclass
 class KernelInput:

gempy_engine/modules/kernel_constructor/_vectors_preparation.py
@@ -8,7 +8,6 @@ from . import _kernel_constructors
 from ._kernel_selectors import dips_sp_cartesian_selector, grid_cartesian_selector
 from . import _structs
 
-
 import numpy as np
 
 
@@ -33,7 +32,6 @@ def cov_vectors_preparation(interp_input: SolverInput) -> _structs.KernelInput:
 
 
 def evaluation_vectors_preparations(grid: np.array, interp_input: SolverInput, axis=None) -> _structs.KernelInput:
-
     sp_: SurfacePointsInternals = interp_input.sp_internal
     ori_: OrientationsInternals = interp_input.ori_internal
     options: InterpolationOptions = interp_input.options
@@ -66,6 +64,7 @@ def _assembly_dips_points_tensors(options, ori_, sp_):
 
 
 def _assembly_dips_points_grid_tensors(grid, options, ori_, sp_):
+    """Used for kernel construction"""
     dips_ref_coord = _kernel_constructors.assembly_dips_points_tensor(ori_.dip_positions_tiled, sp_.ref_surface_points, options)
     dips_rest_coord = _kernel_constructors.assembly_dips_points_tensor(ori_.dip_positions_tiled, sp_.rest_surface_points, options)
     # When we create que core covariance this are the repeated since the distance are with themselves
@@ -75,17 +74,19 @@ def _assembly_dips_points_grid_tensors(grid, options, ori_, sp_):
 
 
 def _assembly_cartesian_selector_tensors(cov_size, options, ori_, sp_):
+    """Used for kernel construction"""
     sel_hu_input, sel_hv_input, sel_hu_points_input = dips_sp_cartesian_selector(cov_size, options.number_dimensions,
                                                                                  ori_.n_orientations, sp_.n_points)
     cartesian_selector = _structs.CartesianSelector(
-        x_sel_hu=sel_hu_input, y_sel_hu=sel_hv_input, x_sel_hv=sel_hv_input,
-        y_sel_hv=sel_hu_input, x_sel_h_ref=sel_hu_points_input,
-        y_sel_h_ref=sel_hu_points_input, x_sel_h_rest=sel_hu_points_input,
-        y_sel_h_rest=sel_hu_points_input)
+        x_sel_hu=sel_hu_input, y_sel_hu=sel_hv_input,
+        x_sel_hv=sel_hv_input, y_sel_hv=sel_hu_input,
+        x_sel_h_ref=sel_hu_points_input, y_sel_h_ref=sel_hu_points_input,
+        x_sel_h_rest=sel_hu_points_input, y_sel_h_rest=sel_hu_points_input)
     return cartesian_selector
 
 
 def _assembly_cartesian_selector_grid(cov_size, grid, options, ori_, sp_, axis=None):
+    """Use for evaluation"""
     sel_hu_input, sel_hv_input, sel_hu_points_input = dips_sp_cartesian_selector(cov_size, options.number_dimensions,
                                                                                  ori_.n_orientations, sp_.n_points,
                                                                                  axis)
@@ -93,18 +94,24 @@ def _assembly_cartesian_selector_grid(cov_size, grid, options, ori_, sp_, axis=N
                                                                            axis)
 
     # TODO: Check I need this
-    a = 1 if axis is None else -1
-
+    a = 1 if axis is None else -1  # ! This is very necessary but also we need to add +1 after (hu_sel_i * -hu_sel_j).sum(-1) 
+    
+    # * I leave this as reference
+    
+    hu_sel_i = sel_hu_input[:, None, :]
+    hu_sel_j = sel_hu_grid[None, :, :]
+    foo = (hu_sel_i * hu_sel_j).sum(-1)  # ! This +1 is only if axis is not None too
+    
     cartesian_selector = _structs.CartesianSelector(
         x_sel_hu=sel_hu_input, y_sel_hu=sel_hu_grid,
         x_sel_hv=sel_hv_input, y_sel_hv=sel_hv_grid,
-        x_sel_h_ref=a * sel_hu_points_input, y_sel_h_ref=sel_hu_points_grid,
+        x_sel_h_ref= sel_hu_points_input, y_sel_h_ref=sel_hu_points_grid,
         x_sel_h_rest=sel_hu_points_input, y_sel_h_rest=sel_hu_points_grid)
     return cartesian_selector
 
 
 def _assembly_drift_tensors(options, ori_, sp_):
-    dips_ug_d1, dips_ug_d2a, dips_ug_d2b, second_degree_selector  = _kernel_constructors.assembly_dips_ug_coords(
+    dips_ug_d1, dips_ug_d2a, dips_ug_d2b, second_degree_selector = _kernel_constructors.assembly_dips_ug_coords(
         ori_, sp_.n_points, options)
     dips_ug = _structs.OrientationsDrift(dips_ug_d1, dips_ug_d1,
                                          dips_ug_d2a, dips_ug_d2a,
@@ -119,13 +126,12 @@ def _assembly_drift_tensors(options, ori_, sp_):
 
     dips_ref_ui = _structs.PointsDrift(dips_ref_d1, dips_ref_d1, dips_ref_d2a, dips_ref_d2a, dips_ref_d2b, dips_ref_d2b)
     dips_rest_ui = _structs.PointsDrift(dips_rest_d1, dips_rest_d1, dips_rest_d2a, dips_rest_d2a, dips_rest_d2b,
-                                       dips_rest_d2b)
+                                        dips_rest_d2b)
 
     return dips_ref_ui, dips_rest_ui, dips_ug
 
 
 def _assembly_drift_grid_tensors(grid, options, ori_, sp_, axis):
-
     # region UG
     dips_ug_d1, dips_ug_d2a, dips_ug_d2b, second_degree_selector = _kernel_constructors.assembly_dips_ug_coords(
         ori_, sp_.n_points, options)
@@ -138,17 +144,17 @@ def _assembly_drift_grid_tensors(grid, options, ori_, sp_, axis):
 
     dips_ug = _structs.OrientationsDrift(dips_ug_d1, grid_1,
                                          dips_ug_d2a, grid * grid_1,
-                                         dips_ug_d2b * sel, grid ,
+                                         dips_ug_d2b * sel, grid,
                                          second_degree_selector)
     # endregion
 
     # region UI
     dips_ref_d1, dips_ref_d2a, dips_ref_d2b = _kernel_constructors.assembly_dips_points_coords(
         sp_.ref_surface_points, ori_.n_orientations_tiled, options)
-    
+
     dips_rest_d1, dips_rest_d2a, dips_rest_d2b = _kernel_constructors.assembly_dips_points_coords(
         sp_.rest_surface_points, ori_.n_orientations_tiled, options)
-    
+
     dips_ref_ui = _structs.PointsDrift(dips_ref_d1, grid, dips_ref_d2a, grid, dips_ref_d2b, grid)
     dips_rest_ui = _structs.PointsDrift(dips_rest_d1, grid, dips_rest_d2a, grid, dips_rest_d2b, grid)
     # endregion
@@ -157,6 +163,5 @@ def _assembly_drift_grid_tensors(grid, options, ori_, sp_, axis):
 
 
 def _new_kernel_input(kernel_input_args) -> _structs.KernelInput:
-
     ki = _structs.KernelInput(*kernel_input_args)
     return ki

test/fixtures/simple_models.py
@@ -106,7 +106,7 @@ def simple_model():
     kri = InterpolationOptions(range_, co, 0,
                                number_dimensions=3, kernel_function=AvailableKernelFunctions.cubic)
     _ = np.ones(3)
-    
+
     tensor_struct = TensorsStructure(np.array([7]))
     stack_structure = StacksStructure(number_of_points_per_stack=np.array([7]),
                                       number_of_orientations_per_stack=np.array([2]),
@@ -268,8 +268,7 @@ def simple_model_values_block_output(simple_model, simple_grid_3d_more_points_gr
 
     ids = np.array([1, 2])
 
-    grid_internal, ori_internal, sp_internal = _input_preprocess(data_shape, grid, orientations,
-                                                                 surface_points)
+    grid_internal, ori_internal, sp_internal = _input_preprocess(data_shape, grid, orientations, surface_points)
     interp_input = SolverInput(sp_internal, ori_internal, options)
 
     weights = _solve_interpolation(interp_input)
@@ -363,7 +362,7 @@ def unconformity_complex_one_layer():
     i_r = 4
     gi_r = 2
 
-    options = InterpolationOptions(range_, c_o, uni_degree=1, i_res=i_r, gi_res=gi_r,
+    options = InterpolationOptions(range_, c_o, uni_degree=0, i_res=i_r, gi_res=gi_r,
                                    number_dimensions=3,
                                    kernel_function=AvailableKernelFunctions.cubic)
 

test/test_common/test_modules/test_exporter.py
@@ -37,20 +37,23 @@ def test_export_scalars(simple_model_values_block_output, plot=True, save_sol=Fa
     # np.testing.assert_almost_equal(gz, gz_sol[:-7], decimal=3)
 
     if plot:
-        # plt.contourf(Z_x.reshape(50, 5, 50)[:, 2, :].T, N=40, cmap="autumn")
-        # plt.colorbar()
-        plt.contourf(Z_x.reshape(50, 5, 50)[:, 2, :].T, N=40, cmap="autumn",
-                     extent=(.25, .75, .25, .75)
-                     )
+        plt.contourf(Z_x.reshape(50, 5, 50)[:, 2, :].T, N=40, cmap="autumn", extent=(.25, .75, .25, .75))
+
+        xyz = output.grid.values
+        every = 10
+        plt.quiver(xyz[::every, 0], xyz[::every, 2], gx[::every], gz[::every], scale=50)
+
+        plt.show()
+
+        plt.contourf(Z_x.reshape(50, 5, 50)[:, 2, :].T, N=40, cmap="autumn", extent=(.25, .75, .25, .75))
 
         xyz = output.grid.values
         every = 1
-        plt.quiver(xyz[::every, 0], xyz[::every, 2], gx[::every], gz[::every], scale =80)
-        
+        plt.quiver(xyz[::every, 0], xyz[::every, 2], gx[::every], gz[::every], scale=80)
+
         plt.show()
 
         plt.contourf(Z_x.reshape(50, 5, 50)[:, 2, :].T, N=40, cmap="autumn")
-        plt.colorbar()
 
         gx_np, gz_np = np.gradient(Z_x.reshape(50, 5, 50)[:, 2, :].T)
         plt.quiver(