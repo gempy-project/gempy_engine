65d13d77ed14287200a5d9d94304569357ed353e
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-05-03 08:54:48 +0200

    [WIP] Trying scipy solvers for pykeops

--- gempy_engine/modules/solver/solver_interface.py
+++ gempy_engine/modules/solver/solver_interface.py
@@ -4,7 +4,7 @@ import gempy_engine.config
 from gempy_engine.core.backend_tensor import BackendTensor, AvailableBackends
 
 import numpy as np
-
+from scipy.sparse.linalg import aslinearoperator, cg
 bt = BackendTensor
 
 
@@ -22,13 +22,17 @@ def kernel_reduction(cov, b, smooth=0.000001):
         case (AvailableBackends.numpy, True):
             # ! Only Positivie definite matrices are solved. Otherwise the kernel gets stuck
             # * Very interesting: https://stats.stackexchange.com/questions/386813/use-the-rbf-kernel-to-construct-a-positive-definite-covariance-matrix
-
-            w = cov.solve(
-                np.asarray(b).astype(dtype),
-                alpha=900000,
-                dtype_acc=dtype,
-                backend="CPU"
-            )
+            
+            if scipy_solver := True:
+                A = aslinearoperator(cov)
+                w, info = cg(A, b[:, 0], tol=0)
+            else:
+                w = cov.solve(
+                    np.asarray(b).astype(dtype),
+                    alpha=.00000,
+                    dtype_acc=dtype,
+                    backend="CPU"
+                )
         case (AvailableBackends.numpy, False):
             if True:
                 cond_number = np.linalg.cond(cov)
