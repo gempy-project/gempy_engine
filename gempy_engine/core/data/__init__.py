2b2586c44fb30707b786226d843ff48233d0b43c
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-07-12 11:26:28 +0200

    - [WIP] Preparing for plotting results

--- gempy_engine/core/data/__init__.py
+++ gempy_engine/core/data/__init__.py
@@ -2,3 +2,4 @@ from .tensors_structure import TensorsStructure
 from .kernel_classes.orientations import Orientations, OrientationsInternals
 from .kernel_classes.surface_points import SurfacePoints, SurfacePointsInternals
 from .options import InterpolationOptions
+from .solutions import Solutions
