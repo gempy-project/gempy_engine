- [BUG] Basement was messing up the masking logic

gempy_engine/API/interp_single/_multi_scalar_field_manager.py
@@ -131,7 +131,7 @@ def _squeeze_mask(all_scalar_fields_outputs: List[ScalarFieldOutput], stack_rela
         if stack_relation[i] is StackRelationType.FAULT:
             mask_matrix[i, :] = all_scalar_fields_outputs[i].mask_components_erode.mask_lith
         if stack_relation[i] is False or stack_relation[i] is StackRelationType.BASEMENT:
-            mask_matrix[i, :] = all_scalar_fields_outputs[i].mask_components_erode.mask_lith
+            mask_matrix[i, :] = all_scalar_fields_outputs[i].mask_components_basement
     
     # Doing the black magic
     final_mask_array     = np.zeros((n_scalar_fields, grid_size), dtype=bool)