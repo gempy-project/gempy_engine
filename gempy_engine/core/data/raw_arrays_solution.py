fa75586dedeb7d683786bb845e1219f019d36ef8
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-16 12:54:29 +0200

    - [ENH] Passing masking arrays to solution

--- gempy_engine/core/data/raw_arrays_solution.py
+++ gempy_engine/core/data/raw_arrays_solution.py
@@ -6,7 +6,10 @@ import numpy as np
 from gempy_engine.core.data.interp_output import InterpOutput
 from gempy_engine.core.data.dual_contouring_mesh import DualContouringMesh
 from gempy_engine.core.data.octree_level import OctreeLevel
-from gempy_engine.modules.octrees_topology.octrees_topology_interface import get_regular_grid_value_for_level, ValueType
+
+# ? These two imports are suggesting that this class should be splatted and move one half into a gempy.module
+from gempy_engine.modules.octrees_topology.octrees_topology_interface import get_regular_grid_value_for_level
+from gempy_engine.modules.octrees_topology.octrees_topology_interface import ValueType
 
 
 @dataclass(init=True)
@@ -26,7 +29,7 @@ class RawArraysSolution:
     scalar_field_matrix: np.ndarray = np.empty(0)
     block_matrix: np.ndarray = np.empty(0)
     mask_matrix: np.ndarray = np.empty(0)
-    mask_matrix_pad: np.ndarray = np.empty(0)
+    mask_matrix_squeezed: np.ndarray = np.empty(0)
     values_matrix: np.ndarray = np.empty(0)
     gradient: np.ndarray = np.empty(0)
     # endregion
@@ -57,16 +60,33 @@ class RawArraysSolution:
     @classmethod
     def from_gempy_engine_solutions(cls, octrees_output: list[OctreeLevel], meshes: list[DualContouringMesh]) \
             -> "RawArraysSolution":
-        legacy_solution = cls()
+        raw_arrays_solution = cls()
 
         # region Blocks
         last_octree_level: OctreeLevel = octrees_output[(-1)]
 
-        legacy_solution._set_block_matrix(octrees_output)
-        legacy_solution._set_lith_block(octrees_output)
-        legacy_solution._set_scalar_field(octrees_output)
+        raw_arrays_solution.block_matrix = cls._get_regular_grid_values_for_all_structural_groups(
+            octree_output=octrees_output, 
+            scalar_type=ValueType.values_block
+        )
+        
+        raw_arrays_solution.scalar_field_matrix = cls._get_regular_grid_values_for_all_structural_groups(
+            octree_output=octrees_output,
+            scalar_type=ValueType.scalar
+        )
+        
+        raw_arrays_solution.mask_matrix = cls._get_regular_grid_values_for_all_structural_groups(
+            octree_output=octrees_output,
+            scalar_type=ValueType.mask_component
+        )
+        
+        raw_arrays_solution.mask_matrix_squeezed = cls._get_regular_grid_values_for_all_structural_groups(
+            octree_output=octrees_output,
+            scalar_type=ValueType.squeeze_mask
+        )
 
-        legacy_solution._set_scalar_field_at_surface_points(last_octree_level)
+        raw_arrays_solution._set_lith_block(octrees_output)
+        raw_arrays_solution._set_scalar_field_at_surface_points(last_octree_level)
         # endregion
         
         # region Grids
@@ -75,48 +95,34 @@ class RawArraysSolution:
         # TODO: Make this more clever to account for the fact that we can have more than one scalar field
         output: InterpOutput = first_level_octree.outputs_centers[-1]  # ! This is the scalar field. Usually we want the last one but not necessarily
         
-        legacy_solution.geological_map = output.geological_map
-        legacy_solution.sections = output.sections
-        legacy_solution.custom = output.custom_grid
+        raw_arrays_solution.geological_map = output.geological_map
+        raw_arrays_solution.sections = output.sections
+        raw_arrays_solution.custom = output.custom_grid
         # endregion
         
         # region Meshes
         if meshes:
-            legacy_solution.vertices = [mesh.vertices for mesh in meshes]
-            legacy_solution.edges = [mesh.edges for mesh in meshes]
+            raw_arrays_solution.vertices = [mesh.vertices for mesh in meshes]
+            raw_arrays_solution.edges = [mesh.edges for mesh in meshes]
             # TODO: I will have to apply the transform to this one
             
         # endregion
-        return legacy_solution
+        return raw_arrays_solution
 
-    def _set_block_matrix(self, octree_output: list[OctreeLevel]):
+    @staticmethod
+    def _get_regular_grid_values_for_all_structural_groups(octree_output: list[OctreeLevel], scalar_type: ValueType):
         temp_list = []
         for i in range(octree_output[0].number_of_outputs):
             temp_list.append(
                 get_regular_grid_value_for_level(
                     octree_list=octree_output,
                     level=None,
-                    value_type=ValueType.scalar,
+                    value_type=scalar_type,
                     scalar_n=i
                 ).ravel()
             )
-        block_matrix_stacked = np.vstack(temp_list)
-        self.block_matrix = block_matrix_stacked
-
-    def _set_scalar_field(self, octree_output:list[OctreeLevel]):
-        temp_list = []
-        for i in range(octree_output[0].number_of_outputs):
-            temp_list.append(
-                get_regular_grid_value_for_level(
-                    octree_list=octree_output,
-                    level=None,
-                    value_type=ValueType.scalar,
-                    scalar_n=i
-                ).ravel()
-            )
-
         scalar_field_stacked = np.vstack(temp_list)
-        self.scalar_field_matrix = scalar_field_stacked
+        return scalar_field_stacked
 
     def _set_scalar_field_at_surface_points(self, octree_output: OctreeLevel):
         temp_list = []
