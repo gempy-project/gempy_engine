2e17ee7323e054024113f91b6aacf85d662a0b05
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-06 11:09:14 +0200

    - [TEST/BUG] Rollback change on octree level for surface

--- gempy_engine/core/data/options.py
+++ gempy_engine/core/data/options.py
@@ -193,7 +193,7 @@ class InterpolationOptions:
     def compute_corners(self):
         is_not_last_octree = (self.is_last_octree_level is False)
         is_dual_contouring = self.dual_contouring
-        is_octree_for_surfaces = self.current_octree_level == self.number_octree_levels_surface 
+        is_octree_for_surfaces = self.current_octree_level == self.number_octree_levels_surface - 1
         is_dual_contouring_and_octree_is_for_surfaces = is_dual_contouring and is_octree_for_surfaces
         
         corners_for_dual_cont = is_dual_contouring_and_octree_is_for_surfaces or is_not_last_octree
@@ -257,7 +257,7 @@ class InterpolationOptions:
     
     @property
     def number_octree_levels_surface(self):
-        if self._number_octree_levels_surface >= self.number_octree_levels:
+        if self._number_octree_levels_surface - 1 >= self.number_octree_levels:
             return self.number_octree_levels
         else:
             return self._number_octree_levels_surface
