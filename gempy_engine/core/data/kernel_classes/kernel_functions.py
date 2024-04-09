ebfd25b54199597df7c1a5978c4f7ca010a7c104
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-04-09 10:26:53 +0200

    [BUG] Numpy arrays sometimes where casting a float64

--- gempy_engine/core/data/kernel_classes/kernel_functions.py
+++ gempy_engine/core/data/kernel_classes/kernel_functions.py
@@ -5,28 +5,38 @@ from typing import Callable
 
 from gempy_engine.core.backend_tensor import BackendTensor
 
+import numpy
+
+dtype = BackendTensor.dtype
+
 
 def cubic_function(r, a):
+    a = float(a)
     return 1 - 7 * (r / a) ** 2 + 35 * r ** 3 / (4 * a ** 3) - 7 * r ** 5 / (2 * a ** 5) + 3 * r ** 7 / (4 * a ** 7)
 
 
 def cubic_function_p_div_r(r, a):
+    a = float(a)
     return (-14 / a ** 2) + 105 * r / (4 * a ** 3) - 35 * r ** 3 / (2 * a ** 5) + 21 * r ** 5 / (4 * a ** 7)
 
 
 def cubic_function_a(r, a):
+    a = float(a)
     return 7 * (9 * r ** 5 - 20 * a ** 2 * r ** 3 + 15 * a ** 4 * r - 4 * a ** 5) / (2 * a ** 7)
 
 
 def exp_function(sq_r, a):
+    a = float(a)
     return BackendTensor.tfnp.exp(-(sq_r / (2 * a ** 2)))
 
 
 def exp_function_p_div_r(sq_r, a):
+    a = float(a)
     return -(1 / (a ** 2) * BackendTensor.tfnp.exp(-(sq_r / (2 * a ** 2))))
 
 
 def exp_function_a(sq_r, a):
+    a = float(a)
     first_term = BackendTensor.tfnp.divide(sq_r, (a ** 4))  # ! This term is almost always zero. I thnk we can just remove it
     second_term = 1 / (a ** 2)
     third_term = BackendTensor.tfnp.exp(-(sq_r / (2 * a ** 2)))
@@ -35,23 +45,25 @@ def exp_function_a(sq_r, a):
 
 square_root_3 = 1.73205080757
 
-
-
 sqrt5 = 2.2360679775
 
 
 def matern_function_5_2(r, a, nu=5 / 2):
     # Using nu=5/2 for the Matern kernel
+
+    a = float(a)
     sqrt5_r_over_ell = sqrt5 * r / a
     return (1 + sqrt5_r_over_ell + (5 * r ** 2) / (3 * a ** 2)) * BackendTensor.tfnp.exp(-sqrt5_r_over_ell)
 
 
 def matern_function_5_2_p_div_r(r, a, nu=5 / 2):
+    a = float(a)
     sqrt5_r_over_ell = sqrt5 * r / a
     return -(5 * BackendTensor.tfnp.exp(-sqrt5_r_over_ell) * (a + sqrt5 * r)) / (3 * a ** 3)
 
 
 def matern_function_5_2_a(r, a, nu=5 / 2):
+    a = float(a)
     sqrt5_r_over_ell = sqrt5 * r / a
     return -5 * BackendTensor.tfnp.exp(-sqrt5_r_over_ell) * (a ** 2 + sqrt5 * a * r - 5 * r ** 2) / (3 * a ** 4)
 
