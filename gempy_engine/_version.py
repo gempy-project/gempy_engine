c295de4596cb8a43256debaf6cbf4b8f7fba75dc
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-04-15 13:40:38 +0200

    [META] releases

--- gempy_engine/_version.py
+++ gempy_engine/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '2024.1.1.dev0+g711c536.d20240415'
-__version_tuple__ = version_tuple = (2024, 1, 1, 'dev0', 'g711c536.d20240415')
+__version__ = version = '2024.1.1'
+__version_tuple__ = version_tuple = (2024, 1, 1)
