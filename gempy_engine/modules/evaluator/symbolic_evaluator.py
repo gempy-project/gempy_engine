71fe7b70b28dd5cbf68c677cc06a9dfd238389db
Miguel de la Varga <miguel@terranigma-solutions.com>
2026-03-13 10:07:09 +0100

    [CLN] Remove unused `symbolic_evaluator_optimized` and outdated helper functions
    - Deleted the `symbolic_evaluator_optimized` function and redundant block-sparse range builders to streamline the codebase.
    - Cleaned up unused logic and comments for better maintainability and clarity.

--- gempy_engine/modules/evaluator/symbolic_evaluator.py
+++ gempy_engine/modules/evaluator/symbolic_evaluator.py
@@ -54,84 +54,6 @@ def symbolic_evaluator(solver_input: SolverInput, weights: np.ndarray, options: 
     return ExportedFields(scalar_field, gx_field, gy_field, gz_field)
 
 
-def symbolic_evaluator_optimized(eval_input: EvaluatorInput, weights: np.ndarray, options: InterpolationOptions):
-    if BackendTensor.engine_backend == gempy_engine.config.AvailableBackends.numpy and eval_input.xyz_to_interpolate.flags['C_CONTIGUOUS'] is False:  # ! This is not working with TF yet
-        print("xyz is not C_CONTIGUOUS")
-    # ! Seems not to make any difference but we need this if we want to change the backend
-    # ! We need to benchmark GPU vs CPU with more input
-    backend_string = BackendTensor.get_backend_string()
-
-    eval_kernel = yield_evaluation_kernel(eval_input, options.kernel_options)
-    if BackendTensor.engine_backend == gempy_engine.config.AvailableBackends.numpy:
-        from pykeops.numpy import LazyTensor
-        # Create lazy_weights with correct dimensions: we want (16, 1) to match eval_kernel's nj dimension
-        lazy_weights = LazyTensor(np.asfortranarray(weights.reshape(-1, 1)), axis=0)  # axis=0 means this is the 'i' dimension
-        scalar_field: np.ndarray = (eval_kernel * lazy_weights).sum(axis=0, backend=backend_string).reshape(-1)
-    else:
-        from pykeops.torch import LazyTensor
-        lazy_weights = LazyTensor(weights.view((-1, 1)), axis=0)  # axis=0 for 'i' dimension
-        # Use element-wise multiplication and sum over the correct axis
-        scalar_field: np.ndarray = (eval_kernel * lazy_weights).sum(axis=0, backend=backend_string).reshape(-1)
-
-    gx_field: Optional[np.ndarray] = None
-    gy_field: Optional[np.ndarray] = None
-    gz_field: Optional[np.ndarray] = None
-
-    if options.compute_scalar_gradient is True:
-        eval_gx_kernel = yield_evaluation_grad_kernel(eval_input, options.kernel_options, axis=0)
-        eval_gy_kernel = yield_evaluation_grad_kernel(eval_input, options.kernel_options, axis=1)
-
-        gx_field = (eval_gx_kernel * lazy_weights).sum(axis=0, backend=backend_string).reshape(-1)
-        gy_field = (eval_gy_kernel * lazy_weights).sum(axis=0, backend=backend_string).reshape(-1)
-
-        if options.number_dimensions == 3:
-            eval_gz_kernel = yield_evaluation_grad_kernel(eval_input, options.kernel_options, axis=2)
-            gz_field = (eval_gz_kernel * lazy_weights).sum(axis=0, backend=backend_string).reshape(-1)
-        elif options.number_dimensions == 2:
-            gz_field = None
-        else:
-            raise ValueError("Number of dimensions have to be 2 or 3")
-
-    return ExportedFields(scalar_field, gx_field, gy_field, gz_field)
-
-
-def _build_block_sparse_ranges_(M_sizes: list[int], N_sizes: list[int]):
-    """Build PyKeOps block-sparse ranges tuple for block-diagonal evaluation.
-    
-    Args:
-        M_sizes: number of evaluation points (j-dimension / grid) per field
-        N_sizes: number of weights/centers (i-dimension / cov_size) per field
-    
-    Returns:
-        ranges: 6-tuple for PyKeOps block-sparse reductions
-    """
-    keep_i = np.cumsum([0] + N_sizes)
-    keep_j = np.cumsum([0] + M_sizes)
-
-    n_fields = len(M_sizes)
-    ranges_i = np.array([[keep_i[k], keep_i[k + 1]] for k in range(n_fields)], dtype=np.int32)
-    slices_i = np.arange(n_fields, dtype=np.int32)
-    ranges_j = np.array([[keep_j[k], keep_j[k + 1]] for k in range(n_fields)], dtype=np.int32)
-    slices_j = np.arange(n_fields, dtype=np.int32)
-
-    return (ranges_i, slices_i, ranges_j, slices_j, ranges_j, slices_j)
-
-
-def _build_block_sparse_ranges_(M_sizes: list[int], N_sizes: list[int]):
-    """Build PyKeOps block-sparse ranges tuple for block-diagonal evaluation."""
-    keep_i = np.cumsum([0] + N_sizes)
-    keep_j = np.cumsum([0] + M_sizes)
-
-    n_fields = len(M_sizes)
-    ranges_i = np.array([[keep_i[k], keep_i[k + 1]] for k in range(n_fields)], dtype=np.int32)
-    slices_i = np.arange(n_fields, dtype=np.int32)
-    ranges_j = np.array([[keep_j[k], keep_j[k + 1]] for k in range(n_fields)], dtype=np.int32)
-    slices_j = np.arange(n_fields, dtype=np.int32)
-
-    # FIXED: Reordered to match PyKeOps expectations
-    # (ranges_i, slices_i, redranges_j, ranges_j, slices_j, redranges_i)
-    return (ranges_i, slices_i, ranges_j, ranges_j, slices_j, ranges_i)
-
 def _build_block_sparse_ranges(M_sizes: list[int], N_sizes: list[int]):
     """Build PyKeOps block-sparse ranges tuple for block-diagonal evaluation."""
     keep_i = np.cumsum([0] + N_sizes)
@@ -162,11 +84,6 @@ def symbolic_evaluator_optimized_stacked(
     from ...modules.kernel_constructor.kernel_constructor_interface import yield_evaluation_kernel, yield_evaluation_grad_kernel
 
     n_fields = len(eval_inputs)
-
-    # If only one field, fall back to the non-stacked version
-    # if n_fields == 1:
-    #     result = symbolic_evaluator_optimized(eval_inputs[0], weights_list[0], options)
-    #     return [result]
 
     backend_string = BackendTensor.get_backend_string()
 
