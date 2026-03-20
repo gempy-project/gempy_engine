ab197da8fe03ec10b6364c5c7b673cab29ea6240
Miguel de la Varga <miguel@terranigma-solutions.com>
2026-03-20 10:13:58 +0100

    [BUG] Fix incorrect conditional logic in gradient evaluation and scalar field interpolation

--- gempy_engine/API/interp_single/_multi_scalar_field_manager.py
+++ gempy_engine/API/interp_single/_multi_scalar_field_manager.py
@@ -26,7 +26,7 @@ def interpolate_all_fields(interpolation_input: InterpolationInput, options: Int
                            data_descriptor: InputDataDescriptor) -> List[InterpOutput]:
     """Interpolate all scalar fields given a xyz array of points"""
 
-    if False:
+    if True:
         all_scalar_fields_outputs: List[ScalarFieldOutput] = _interpolate_stack(data_descriptor, interpolation_input, options)
     else:
         all_scalar_fields_outputs: List[ScalarFieldOutput] = _interpolate_stack_flat(data_descriptor, interpolation_input, options)
