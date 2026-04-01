eed040f8e6a2bf02fec8bc4885e31d70a1c64733
Miguel de la Varga <miguel@terranigma-solutions.com>
2026-04-01 19:58:46 +0200

    [ENH] Add `pykeops` support to kernel evaluation and gradient computation
    - Updated `yield_evaluation_kernel` and `yield_evaluation_grad_kernel` to include an optional `pykeops` parameter.
    - Integrated `upgrade_tensors` logic when `pykeops` is enabled.
    - Modified symbolic evaluator to utilize `pykeops` in scalar and gradient kernel workflows.

--- gempy_engine/API/interp_single/_multi_scalar_field_manager.py
+++ gempy_engine/API/interp_single/_multi_scalar_field_manager.py
@@ -28,7 +28,7 @@ def interpolate_all_fields(interpolation_input: InterpolationInput, options: Int
                            data_descriptor: InputDataDescriptor) -> List[InterpOutput]:
     """Interpolate all scalar fields given a xyz array of points"""
 
-    if os.getenv("GEMPY_FLAT_STACKS", "False").lower() in ("true", "1", "t", "y", "yes"):
+    if os.getenv("GEMPY_FLAT_STACKS", "False").lower() in ("true", "1", "t", "y", "yes") and BackendTensor.use_pykeops:
         all_scalar_fields_outputs: List[ScalarFieldOutput] = _interpolate_stack_flat(data_descriptor, interpolation_input, options)
     else:
         all_scalar_fields_outputs: List[ScalarFieldOutput] = _interpolate_stack(data_descriptor, interpolation_input, options)
